4c776efa373d571213c87f0185af3f47fecde3d5
SUPRITA <SUPRITA@LAPTOP-C6BNCV2K>
2023-06-08 12:02:48 +0530

    SECOND COMMIT

---
+++ Suprita/src/test/java/sup/SureFireTest.java
@@ -0,0 +1,16 @@
+package sup;
+import org.testng.annotations.Test;
+
+public class SureFireTest {
+
+	@Test
+	public void test() {
+		int i=10;
+		int j=20;
+		add(i,j);}
+		
+		public void add(int i, int j) {
+		int k=i+j;
+		System.out.println(k);
+	}
+}
